8c638039ab0ff824e7e8d4e5d9f829d73b875721
Вик30то <viktor81rovalchuk@gmail.com>
2022-12-07 21:38:26 +0200

    домашка 1

---
+++ Raschet.py
@@ -0,0 +1,19 @@
+fuel_consumption_car: int = 100
+price_of_one_liter_gasoline: float =50.83
+passed: int =110
+
+user_choise = input ("""выберите одно:
+1. Расход топлива автомобилем 
+2. Цена одного литра бензина 
+3. Сколько нужно проехать 
+4. Сколько уйдет денег
+>>>""")
+user_money = input("""Сколько денег уйдет ?
+>>>""")
+user_money = float(user_money)
+a = 100
+b = 50.83
+c = 110
+print(a*b/c)
+
+
